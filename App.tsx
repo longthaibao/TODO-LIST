54d87635b862a241dd4025cb5bc968c6e2c5d1e4
Thái Bảo Long <subin27102003@gmail.com>
2024-01-17 21:31:59 +0700

    Hoan thanh app todo list

--- App.tsx
+++ App.tsx
@@ -1,118 +1,151 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState} from 'react';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
+  Alert,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TextInput,
+  TouchableOpacity,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+const App = () => {
+  const [joblist, setJoblist] = useState<String[]>([]);
+  const [job, setJob] = useState('');
+  const handleChange = (text: any) => {
+    setJob(text);
   };
-
+  const handlePress = () => {
+    if (job == '') {
+      Alert.alert('Bạn chưa nhập task', 'Vui lòng nhập task');
+    } else {
+      setJoblist(prevJoblist => [...prevJoblist, job]);
+      setJob('');
+    }
+  };
+  const handleDelete = (index: number) => {
+    Alert.alert('Xoá công việc', 'Bạn có chắc chắn muốn xoá không?', [
+      {
+        text: 'Yes',
+        onPress: () => {
+          if (index === joblist.length - 1) {
+            setJoblist(prev => prev.slice(0, prev.length - 1));
+          } else {
+            setJoblist(prev => prev.filter((_, i) => i !== index));
+          }
+        },
+      },
+      {
+        text: 'Cancel',
+        onPress: () => {
+          return;
+        },
+      },
+    ]);
+  };
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.title}>TODO LIST</Text>
+        <ScrollView>
+          {joblist.map((element, index) => {
+            return (
+              <TouchableOpacity key={index} onPress={() => handleDelete(index)}>
+                <View style={styles.item}>
+                  <View style={styles.icon}>
+                    <Text>{index}</Text>
+                  </View>
+                  <Text>{element}</Text>
+                </View>
+              </TouchableOpacity>
+            );
+          })}
+        </ScrollView>
+      </View>
+      <View style={styles.InputButton}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your task"
+          value={job}
+          onChangeText={text => handleChange(text)}
+        />
+        <TouchableOpacity onPress={handlePress}>
+          <View style={styles.button}>
+            <Text>+</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
-}
-
+};
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    backgroundColor: '#eff7f8',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  content: {
+    marginVertical: 60,
+    marginHorizontal: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  title: {
+    fontSize: 25,
+    color: 'blue',
+    fontWeight: '800',
   },
-  highlight: {
-    fontWeight: '700',
+  item: {
+    height: 50,
+    width: '90%',
+    backgroundColor: 'white',
+    marginVertical: 7,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+    borderRadius: 15,
+  },
+  icon: {
+    height: 40,
+    width: 45,
+    borderRadius: 10,
+    backgroundColor: 'cyan',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 10,
+  },
+  footer: {
+    height: 70,
+  },
+  textInput: {
+    height: 60,
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderColor: 'cyan',
+    marginHorizontal: 7,
+    borderRadius: 20,
+    flex: 7,
+    backgroundColor: 'white',
+  },
+  InputButton: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  button: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'cyan',
+    borderRadius: 70,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
   },
 });
-
 export default App;
